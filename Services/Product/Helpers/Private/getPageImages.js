00665848a5b18a74099034e6022fbc1cf45622fb
Michael Bouclas <mbouclas@gmail.com>
2015-10-04 11:38:32 +0300

    fixes - additions

--- Services/Product/Helpers/Private/getPageImages.js
+++ Services/Product/Helpers/Private/getPageImages.js
@@ -1,6 +1,6 @@
 module.exports = (function(App,Connection,Package){
     return function(ids,callback){
-        return App.Helpers.MongoDB.itemImages(App.Connections[App.Config.database.default].models.ProductImage,ids,{},function(err,res){
+        return App.Helpers.MongoDB.itemImages(App.Connections[App.Config.database.default].models.ProductImage,ids,{active : true},function(err,res){
             callback(null,res);
         });
     }
